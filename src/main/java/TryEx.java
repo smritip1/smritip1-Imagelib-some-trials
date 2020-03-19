32ee77246151a81a1c8fe13bddbae332733d5659
smritip1 <19982783+smritip1@users.noreply.github.com>
2020-03-19 00:13:49 -0400

    Add files via upload
    Changed the input to RealType

--- src/main/java/TryEx.java
+++ src/main/java/TryEx.java
@@ -4,82 +4,80 @@ import io.scif.img.IO;
 import io.scif.img.ImgIOException;
 
 import net.imglib2.Cursor;
-import net.imglib2.RandomAccess;
 import net.imglib2.img.Img;
-import net.imglib2.img.ImgFactory;
-import net.imglib2.img.array.ArrayImgFactory;
-import net.imglib2.img.cell.CellImgFactory;
 import net.imglib2.img.display.imagej.ImageJFunctions;
-import net.imglib2.type.Type;
-import net.imglib2.type.numeric.real.FloatType;
-
+import net.imglib2.type.NativeType;
+import net.imglib2.type.numeric.RealType;
 /**
- * Opens a file with ImgOpener and threshold it based on a pre-defined threshold value
+ * Opens a file with ImgOpener and threshold it based on a pre-defined value
  * All the pixels in the input image which are less than or equal to 180 are set to 0 (black) and
  * the pixels above 180 are set to 255 (white)
  * 
  * */
 public class TryEx {
 
-	public TryEx() 
-	{
-		// open with ImgOpener
-		Img< FloatType > img = IO.openImgs( "DrosophilaWing.tif", new ArrayImgFactory<>( new FloatType() ) ).get( 0 );
-		
-		ImageJFunctions.show( img );
-		
-		// Image Threshold function
-		Img< FloatType > imgThresh = imageThreshold( img, new CellImgFactory<>( new FloatType(), 20 ) );
+	//private double threshold;
 
-		// show the image
-		//ImageJFunctions.show( img );
-		ImageJFunctions.show( imgThresh );
+	public < T extends RealType< T > & NativeType< T > > TryEx() throws ImgIOException
+	{
+		
+		// open with ImgOpener
+        final Img< T > img = (Img< T >) IO.openImgs( "DrosophilaWing.tif" ).get( 0 );
+        ImageJFunctions.show( img );
+        
+        // Image Threshold function
+        Img< T > output = imageThreshold( img );
+ 
+        // display the copy
+        ImageJFunctions.show( output );
 		
 	}
 	
-	public < T extends Type< T > > Img< T > imageThreshold( final Img< T > input,
-		    final ImgFactory< T > imgFactory )
-	{
-		    // create a new Image with the same properties as input
-			Img< T > output = input.factory().create( input );
-
-			// create a cursor that automatically localizes itself on every move
-		    Cursor< T > cursorInput = input.localizingCursor();
-		    RandomAccess< T > randomAccess = output.randomAccess();
+	public < T extends RealType< T > > Img< T > imageThreshold( final Img< T > input ) throws ImgIOException
+    {
+		// create a new Image with the same properties
+        // note that the input provides the size for the new image as it implements
+        // the Interval interface
+        Img< T > output = input.factory().create( input );
+ 
+        // create a cursor for both images
+        Cursor< T > cursorInput = input.cursor();
+        Cursor< T > cursorOutput = output.cursor();
 			
-			// define threshold
-			float threshold = 180;
-
-			// iterate over the input pixels
-			while ( cursorInput.hasNext())
-			{
-				// move both cursors forward by one pixel
-				cursorInput.fwd();
-				// set the output cursor to the position of the input cursor
-			    randomAccess.setPosition( cursorInput );
-		        
-				// get the value of the next pixel in the input image
-				FloatType thisPixel = (FloatType) cursorInput.next();
-				
-				// check if the value of pixel is above the threshold
-				if ( thisPixel.get() > threshold )
-				{
-					//set the current pixel to 255 in the input image
-					thisPixel.set( 255 );
-					// set the value of this pixel of the output image, every Type supports T.set( T type )
-				    randomAccess.get().set(cursorInput.get() );
-				}
-				else
-				{
-					//set the current pixel to 0 in the input image
-					thisPixel.set( 0 );
-					// set the value of this pixel of the output image, every Type supports T.set( T type )
-				    randomAccess.get().set( cursorInput.get() );
-				}
-			}
+        double threshold = 180.0;
+        
+		// iterate over the input pixels
+		while ( cursorInput.hasNext())
+		{
+			// move both cursors forward by one pixel
+			cursorInput.fwd();
+			cursorOutput.fwd();
+			
+			
+			final RealType<?> thisPixel = cursorInput.get();
+          
+            if (thisPixel.getRealDouble() > threshold)
+            {
+            	//set the current pixel to 255 in the input image
+            	thisPixel.setReal(255.0);
+            	
+            	// set the value of this pixel of the output image
+            	cursorOutput.get().set( cursorInput.get() );
+            	
+            }
+            else
+            {
+            	//set the current pixel to 0 in the input image
+            	thisPixel.setReal(0.0);
+            	
+            	// set the value of this pixel of the output image
+            	cursorOutput.get().set( cursorInput.get() );
+            }
+            
+		}
 					
-				// return the copy
-				return output;
+		// return the output image
+		return output;
     }
 			
 	public static void main(String[] args) 
